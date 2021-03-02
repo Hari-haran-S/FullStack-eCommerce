b842eebd18b26f5321a3d8772f0f225864f7266f
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-03-02 15:25:10 +0530

    before Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-
+import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
     <>
-      <h1>Hey there!</h1>
+      <Header />
+      <main className='py-3'>
+      <Container>
+          <HomeScreen />
+      </Container>
+      </main>
+      <Footer />
     </>
   );
 }
