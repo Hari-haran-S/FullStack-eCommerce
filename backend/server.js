827fb44c3702c6481ac212cedd858d804d69fceb
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-04-02 12:37:02 +0530

    error handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import colors from 'colors'
+import {notFound, errhandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js' 
 
 dotenv.config()
 connectDB();
@@ -10,13 +11,10 @@ const app = express();
 app.get('/', (req,res) => {
     res.send('API is running....vvv')
 })
-app.get('/api/products', (req,res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+
+app.use('/api/products', productRoutes)
+app.use(notFound);
+app.use(errhandler);
 
 const port = process.env.PORT || 8000
 app.listen(port, console.log(`Server Runnig in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold));
