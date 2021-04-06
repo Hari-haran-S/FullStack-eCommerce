5528e5377ffa2c2c3d2ae892c60db4c07a8b39c3
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-04-06 15:33:25 +0530

    Paypal Implementation

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItem  } from '../controllers/orderController.js'
+import { addOrderItem, getOrderID, updateOrdertoPaid  } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItem)
+router.route('/:id').get(protect, getOrderID)
+router.route('/:id/pay').put(protect, updateOrdertoPaid)
 
 export default router
