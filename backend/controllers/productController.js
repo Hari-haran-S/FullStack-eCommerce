d598d267dc20abaad666c4b7415882681511303b
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-04-10 00:07:52 +0530

    image upload

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,5 +15,53 @@ const getProductsById = asyncHandler(async (req,res) => {
         throw new Error(`Product not Found`)
     }
 })
+const deleteProduct = asyncHandler(async (req,res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+       await product.remove()
+       res.json({message:'product deleted'})
+    } else {
+        res.status(404)
+        throw new Error(`Product not Found`)
+    }
+})
+const createProduct = asyncHandler(async (req,res) => {
+   const product = new Product({
+       name: 'Sample Name',
+       price: 0,
+       user: req.user._id,
+       image: '/images/sample.jpg',
+       brand: 'Sample brand',
+       category: 'Sample category',
+       countInStock: 0,
+       numReviews: 0,
+       description: 'Sample description'
+   })
 
-export {getProducts, getProductsById}
+   const createdProduct = await product.save()
+   res.status(201).json(createdProduct)
+})
+
+const updateProduct = asyncHandler(async (req,res) => {
+     const { name, price, description, image, category,brand, countInStock} = req.body
+     const product = await Product.findById(req.params.id)
+     if(product){
+         product.name = name,
+         product.price = price,
+         product.description = description,
+         product.category = category,
+         product.brand = brand,
+         product.countInStock = countInStock,
+         product.image = image
+
+         const updatedProduct = await product.save()
+         res.status(201).json(updatedProduct)
+
+     }else{
+         res.status(404)
+         throw new Error(`Product not found`);
+
+     }
+ })
+
+export {getProducts, getProductsById, deleteProduct, createProduct, updateProduct}
