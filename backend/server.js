569c320d726c21771e6886e24a0525437857d317
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-04-12 18:27:39 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,7 @@ if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 app.use(express.json())
-app.get('/', (req,res) => {
-    res.send('API is running....vvv')
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -28,8 +26,22 @@ app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) =>{
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
 const __dirname = path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}else {
+    app.get('/', (req,res) => {
+        res.send('API is running....vvv')
+    })
+}
+
 app.use(notFound);
 app.use(errhandler);
 
